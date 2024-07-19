f39882c0820af1aa1ca4b74b56201017b5a344ec
abazunts <bazunc@gmail.com>
2024-07-19 21:34:39 +0400

    added run deploy-production.yaml

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 const app = (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.end('Hello, World!!\n');
 };
 
 let server;
